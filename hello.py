75c4699d39baf43095e617e77a9420e373586042
Manish Kumar <malikrajw7@gmail.com>
2026-06-14 01:08:15 +0530

    Day 1 Variable

--- hello.py
+++ hello.py
@@ -5,3 +5,46 @@ Height = 5.11
 print("Name", name)
 print("age", age)
 print("Height", Height)
+
+#Sum of two numbers 
+a= 234
+b= 891
+print(a+b)
+
+#Sum of tree numbers 
+a=5
+b=4
+c=6
+print(a+b+c)
+
+#student marks 
+english= 76
+hindi = 78
+maths= 89
+print (english+hindi+maths)
+
+#Area of rectangle 
+length = 3
+width= 6
+area = length*width
+print(area)
+
+#enter input sum 
+
+a = int(input ("enter first number :"))
+b = int(input("enter second number :"))
+sum = a+b
+print(sum)
+
+#write a program that print your details using veraible 
+Name = "Manish"
+Age = 21
+Rollno = 232077098
+location= "ludhiana"
+birthplace= "ludhiana"
+
+print("name:", Name )
+print("Age:" ,Age )
+print("Rollno:", Rollno)
+print("location:", location)
+print("birthplace:", birthplace )
