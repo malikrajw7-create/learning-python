b586209afed53ded07371c225ad2e135546b874c
Manish Kumar <malikrajw7@gmail.com>
2026-06-15 00:09:43 +0530

    Day 2 data types, keywords , tokens

--- hello2.py
+++ hello2.py
@@ -2,25 +2,29 @@
     Sum
     Data type of first number
     Data type of second number'''
-a = int(input("Enter the first number :"))
-b = int(input("Enter the first number :"))
-total = a + b
-print(total)
-print(type(a))
-print(type(b))
+# a = int(input("Enter the first number :"))
+# b = int(input("Enter the first number :"))
+# total = a + b
+# print(total)
+# print(type(a))
+# print(type(b))
 
 #print all the types of this line of code
-a = 10
-b = 20.5
-c = "Python"
-d = True
-price = 99.99
-quantity = 5 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(price))
-print(type(quantity))
+# a = 10
+# b = 20.5
+# c = "Python"
+# d = True
+# price = 99.99
+# quantity = 5 
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(price))
+# print(type(quantity))
 
+# how to finf any words is keyword or not 
+import keyword
+print(keyword.iskeyword("if"))
+print(keyword.kwlist)
 
